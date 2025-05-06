f1951b61803411d817c0ea4432d7e9b30af8d516
Andy Ansong <andyansong82@gmail.com>
2025-05-06 04:11:20 +0100

    set video

--- src/App.jsx
+++ src/App.jsx
@@ -5,6 +5,17 @@ function App() {
   const photoRef = useRef(null)
   const [hasPhoto, setHasPhoto] = useState(false)
 
+  const getVideo = () => {
+    navigator.mediaDevices.getUserMedia({
+      video: {width: 1920, height: 1080}
+    })
+    .then(stream => {
+      let video = videoRef.current
+      video.srcObject = stream
+      video.play()
+    })
+  }
+
   return (
     <div className="App">
       <div className="camera">
