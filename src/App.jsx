d75596797dd0b76ecd5e20b3c6e8c72e5ae63549
Andy Ansong <andyansong82@gmail.com>
2025-05-06 04:23:51 +0100

    setup camera

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { useRef, useState } from 'react'
+import { useRef, useState, useEffect } from 'react'
 
 function App() {
   const videoRef = useRef(null)
@@ -14,17 +14,47 @@ function App() {
       video.srcObject = stream
       video.play()
     })
+    .catch(err => {
+      console.error(err)
+    })
   }
+
+  const takePhoto = () => {
+    const width = 414
+    const height = width / (16/9)
+
+    let video = videoRef.current
+    let photo = photoRef.current
+
+    photo.width = width
+    photo.height = height
+
+    let ctx = photo.getContext('2d')
+    ctx.drawImage(video, 0, 0, width, height)
+    setHasPhoto(true)
+  }
+
+  const closePhoto = () => {
+    let photo = photoRef.current
+    let ctx = photo.getContext('2d')
+
+    ctx.clearRect(0,0,photo.width,photo.height)
+    setHasPhoto(false)
+  }
+
+  useEffect(() => {
+    getVideo()
+  }, [videoRef])
 
   return (
     <div className="App">
       <div className="camera">
         <video ref={videoRef}></video>
-        <button>SNAP</button>
+        <button onClick={takePhoto}>SNAP</button>
       </div>
       <div className={"result" + (hasPhoto ? "hasPhoto" : "")}>
         <canvas ref={photoRef}></canvas>
-        <button>CLOSE</button>
+        <button onClick={closePhoto}>CLOSE</button>
       </div>
     </div>
   )
